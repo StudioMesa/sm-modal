09728d7a908ea4d36967afaeb9b9df77f365b643
Studio Mesa <88522831+StudioMesa@users.noreply.github.com>
2025-04-17 22:46:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
   document.addEventListener("DOMContentLoaded", function () {
     const modals = document.querySelectorAll(".custom-modal");
     const closeButtons = document.querySelectorAll(".custom-modal-close");
-    let scrollY = 0;
 
     function openModal(modal) {
-      scrollY = window.scrollY;
       document.body.classList.add("modal-lock");
-      document.body.style.top = `-${scrollY}px`;
       modal.classList.add("open");
     }
 
@@ -15,20 +12,18 @@
       setTimeout(() => {
         modal.classList.remove("open", "closing");
         document.body.classList.remove("modal-lock");
-        document.body.style.top = "";
-        window.scrollTo(0, scrollY);
       }, 300);
     }
 
     document.querySelectorAll("a[href^='#']").forEach(link => {
-      link.addEventListener("click", e => {
-        const id = link.getAttribute("href").slice(1);
-        const modal = document.getElementById(id);
-        if (modal && modal.classList.contains("custom-modal")) {
+      const id = link.getAttribute("href").slice(1);
+      const modal = document.getElementById(id);
+      if (modal && modal.classList.contains("custom-modal")) {
+        link.addEventListener("click", function (e) {
           e.preventDefault();
           openModal(modal);
-        }
-      });
+        });
+      }
     });
 
     closeButtons.forEach(btn => {
